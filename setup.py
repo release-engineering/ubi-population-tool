566e1582b7f50b65e420c32085200895e418b041
Robert Bikar <rbikar@redhat.com>
2019-03-19 14:23:33 +0100

    Raise version to 0.1.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def get_requirements():
 
 
 setup(name='ubi-population-tool',
-      version='0.0.1',
+      version='0.1.1',
       license='GNU General Public License',
       author='',
       author_email='',
