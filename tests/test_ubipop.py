48e50d4e08a93adeb94bea52d9bb2725fd1cd593
rbikar <31000668+rbikar@users.noreply.github.com>
2019-04-03 14:41:22 +0200

    Refactor associations and unassociations, add test coverage, renamed some attributes (#35)
    Refactored and added more abstraction for un/associations code
    to be more readable.
    Also renamed few attributes and variables.
    Added missing test coverage for affected code in this pr.

--- tests/test_ubipop.py
+++ tests/test_ubipop.py
@@ -3,8 +3,11 @@ import ubiconfig
 import tempfile
 import os
 import shutil
+import logging
+import sys
 from ubipop import UbiPopulateRunner, UbiRepoSet, RepoSet, UbiPopulate
-from ubipop._pulp_client import Module, Package
+from ubipop._pulp_client import Module, Package, Repo
+from ubipop._utils import AssociateActionModules, UnassociateActionModules
 from mock import MagicMock
 from mock import patch
 from more_executors import Executors
@@ -32,6 +35,11 @@ def executor():
 @pytest.fixture()
 def mock_ubipop_runner(ubi_repo_set, test_ubiconfig, executor):
     yield UbiPopulateRunner(MagicMock(), ubi_repo_set, test_ubiconfig, False, executor)
+
+
+def get_test_repo(**kwargs):
+    return Repo(kwargs.get('repo_id'), kwargs.get('arch'), kwargs.get('platform_full_version'),
+                kwargs.get('distributors_ids_type_ids'))
 
 
 def get_test_pkg(**kwargs):
@@ -133,8 +141,8 @@ def test_match_packages(mock_ubipop_runner):
     mock_ubipop_runner.pulp.search_rpms.return_value = [get_test_pkg(name=package_name)]
     mock_ubipop_runner._match_packages()
 
-    assert len(mock_ubipop_runner.out_repo_set.packages) == 1
-    assert mock_ubipop_runner.out_repo_set.packages[package_name][0].name == package_name
+    assert len(mock_ubipop_runner.repos.packages) == 1
+    assert mock_ubipop_runner.repos.packages[package_name][0].name == package_name
 
 
 def test_match_modules(mock_ubipop_runner):
@@ -145,10 +153,10 @@ def test_match_modules(mock_ubipop_runner):
 
     mock_ubipop_runner._match_modules()
 
-    assert len(mock_ubipop_runner.out_repo_set.modules) == 1
-    assert len(mock_ubipop_runner.out_repo_set.modules["n1s1"]) == 1
-    assert mock_ubipop_runner.out_repo_set.modules["n1s1"][0].name == 'm1'
-    pkg = mock_ubipop_runner.out_repo_set.pkgs_from_modules["n1s1"][0]
+    assert len(mock_ubipop_runner.repos.modules) == 1
+    assert len(mock_ubipop_runner.repos.modules["n1s1"]) == 1
+    assert mock_ubipop_runner.repos.modules["n1s1"][0].name == 'm1'
+    pkg = mock_ubipop_runner.repos.pkgs_from_modules["n1s1"][0]
     assert pkg.filename == "tomcatjss-7.3.6-1.el8+1944+b6c8e16f.noarch.rpm"
     assert pkg.filename == "tomcatjss-7.3.6-1.el8+1944+b6c8e16f.noarch.rpm"
 
@@ -183,8 +191,8 @@ def test_keep_n_newest_packages_with_referenced_pkg_in_module(mock_ubipop_runner
         get_test_pkg(name="tomcatjss", filename="tomcatjss-7.3.7-1.el8+1944+b6c8e16f.noarch.rpm"),
         get_test_pkg(name="tomcatjss", filename="tomcatjss-7.3.8-1.el8+1944+b6c8e16f.noarch.rpm")]
 
-    mock_ubipop_runner.out_repo_set.modules["ns"] = []
-    mock_ubipop_runner.out_repo_set.pkgs_from_modules["ns"] = \
+    mock_ubipop_runner.repos.modules["ns"] = []
+    mock_ubipop_runner.repos.pkgs_from_modules["ns"] = \
         [get_test_pkg(name="tomcatjss", filename="tomcatjss-7.3.7-1.el8+1944+b6c8e16f.noarch.rpm")]
 
     mock_ubipop_runner.keep_n_newest_packages(packages)
@@ -246,7 +254,7 @@ def test_ubipopulate_load_all_ubiconfig(mocked_ubiconfig_load_all):
 
 def test_create_srpms_output_set(mock_ubipop_runner):
     expected_src_rpm_filename = "tomcatjss-7.3.6-1.el8+1944+b6c8e16f.src.rpm"
-    mock_ubipop_runner.out_repo_set.packages['foo'] = \
+    mock_ubipop_runner.repos.packages['foo'] = \
         [get_test_pkg(name="tomcatjss",
                       filename="tomcatjss-7.3.6-1.el8+1944+b6c8e16f.noarch.rpm"),
          get_test_pkg(name="kernel",
@@ -254,7 +262,7 @@ def test_create_srpms_output_set(mock_ubipop_runner):
          ]
 
     mock_ubipop_runner._create_srpms_output_set()
-    out_srpms = mock_ubipop_runner.out_repo_set.source_rpms
+    out_srpms = mock_ubipop_runner.repos.source_rpms
     assert len(out_srpms) == 1
     assert out_srpms[0].name == "tomcatjss"
     assert out_srpms[0].filename == expected_src_rpm_filename
@@ -262,7 +270,7 @@ def test_create_srpms_output_set(mock_ubipop_runner):
 
 def test_create_debug_output_set(mock_ubipop_runner):
     expected_debug_filename = "tomcatjss-debuginfo-7.3.6-1.el8+1944+b6c8e16f.noarch.rpm"
-    mock_ubipop_runner.out_repo_set.packages['foo'] = \
+    mock_ubipop_runner.repos.packages['foo'] = \
         [get_test_pkg(name="tomcatjss",
                       filename="tomcatjss-7.3.6-1.el8+1944+b6c8e16f.noarch.rpm"),
          get_test_pkg(name="kernel",
@@ -270,7 +278,7 @@ def test_create_debug_output_set(mock_ubipop_runner):
          ]
 
     mock_ubipop_runner._create_debuginfo_output_set()
-    out_debug_rpms = mock_ubipop_runner.out_repo_set.debug_rpms
+    out_debug_rpms = mock_ubipop_runner.repos.debug_rpms
     assert len(out_debug_rpms) == 1
     assert out_debug_rpms[0].name == "tomcatjss"
     assert out_debug_rpms[0].filename == expected_debug_filename
@@ -308,3 +316,137 @@ def test_create_output_file(mock_ubipop_runner, mock_get_repo_pairs, mocked_ubic
 
     finally:
         shutil.rmtree(path)
+
+
+@pytest.fixture()
+def mock_current_content_ft():
+    current_modules_ft = MagicMock()
+    current_rpms_ft = MagicMock()
+    current_srpms_ft = MagicMock()
+    current_debug_rpms_ft = MagicMock()
+
+    current_modules_ft.result.return_value = [get_test_mod(name="md_current")]
+    current_rpms_ft.result.return_value = [get_test_pkg(name="rpm_current",
+                                                        filename="rpm_current.rpm")]
+    current_srpms_ft.result.return_value = [get_test_pkg(name="srpm_current",
+                                                         filename="srpm_current.src.rpm")]
+    current_debug_rpms_ft.result.return_value = [get_test_pkg(name="debug_rpm_current",
+                                                              filename="debug_rpm_current.rpm")]
+
+    yield current_modules_ft, current_rpms_ft, current_srpms_ft, current_debug_rpms_ft
+
+
+def test_get_pulp_actions(mock_ubipop_runner, mock_current_content_ft):
+    mock_ubipop_runner.repos.modules = {"test": [get_test_mod(name="test_md")]}
+    mock_ubipop_runner.repos.packages = {"test_rpm": [get_test_pkg(name="test_rpm",
+                                                                   filename="test_rpm.rpm")]}
+    mock_ubipop_runner.repos.debug_rpms = {"test_debug_pkg":
+                                           [get_test_pkg(name="test_debug_pkg",
+                                                         filename="test_debug_pkg.rpm")]}
+    mock_ubipop_runner.repos.source_rpms = [get_test_pkg(name="test_srpm",
+                                                         filename="test_srpm.src.rpm")]
+
+    associations, unassociations = \
+        mock_ubipop_runner._get_pulp_actions(*mock_current_content_ft)
+
+    # firstly, check correct associations, there should 1 unit of each type associated
+    modules, rpms, srpms, debug_rpms = associations
+    assert len(modules.units) == 1
+    assert modules.units[0].name == "test_md"
+    assert len(rpms.units) == 1
+    assert rpms.units[0].name == "test_rpm"
+    assert len(srpms.units) == 1
+    assert srpms.units[0].name == "test_srpm"
+    assert len(debug_rpms.units) == 1
+    assert debug_rpms.units[0].name == "test_debug_pkg"
+
+    # secondly, check correct unassociations, there should 1 unit of each type unassociated
+    modules, rpms, srpms, debug_rpms = unassociations
+    assert len(modules.units) == 1
+    assert modules.units[0].name == "md_current"
+    assert len(rpms.units) == 1
+    assert rpms.units[0].name == "rpm_current"
+    assert len(srpms.units) == 1
+    assert srpms.units[0].name == "srpm_current"
+    assert len(debug_rpms.units) == 1
+    assert debug_rpms.units[0].name == "debug_rpm_current"
+
+
+def test_get_pulp_actions_no_actions(mock_ubipop_runner, mock_current_content_ft):
+    mock_ubipop_runner.repos.modules = {"test": [get_test_mod(name="md_current")]}
+    mock_ubipop_runner.repos.packages = {"test_rpm": [get_test_pkg(name="rpm_current",
+                                                      filename="rpm_current.rpm")]}
+    mock_ubipop_runner.repos.debug_rpms = [get_test_pkg(name="debug_rpm_current",
+                                                         filename="debug_rpm_current.rpm")]
+    mock_ubipop_runner.repos.source_rpms = {"test_debug_pkg": [get_test_pkg(name="srpm_current",
+                                                              filename="srpm_current.src.rpm")]}
+
+    associations, unassociations = \
+        mock_ubipop_runner._get_pulp_actions(*mock_current_content_ft)
+
+    # firstly, check correct associations, there should 0 units associated
+    modules, rpms, srpms, debug_rpms = associations
+    assert len(modules.units) == 0
+    assert len(rpms.units) == 0
+    assert len(srpms.units) == 0
+    assert len(debug_rpms.units) == 0
+
+    # secondly, check correct unassociations, there should 0 units unassociated
+    modules, rpms, srpms, debug_rpms = unassociations
+    assert len(modules.units) == 0
+    assert len(rpms.units) == 0
+    assert len(srpms.units) == 0
+    assert len(debug_rpms.units) == 0
+
+
+@pytest.fixture()
+def set_logging():
+    logger = logging.getLogger("ubipop")
+    logger.setLevel(logging.DEBUG)
+    yield logger
+    logger.handlers = []
+
+
+def test_log_pulp_action(capsys, set_logging, mock_ubipop_runner):
+    set_logging.addHandler(logging.StreamHandler(sys.stdout))
+    src_repo = get_test_repo(repo_id='test_src')
+    dst_repo = get_test_repo(repo_id='test_dst')
+    associations = [AssociateActionModules([get_test_mod(name="test_assoc")], dst_repo, src_repo)]
+    unassociations = [UnassociateActionModules([get_test_mod(name="test_unassoc")], dst_repo)]
+
+    mock_ubipop_runner.log_pulp_actions(associations, unassociations)
+    out, err = capsys.readouterr()
+    assoc_line, unassoc_line = out.split('\n', 1)
+
+    assert err == ""
+    assert assoc_line.strip() == "Would associate test_assoc:::: from test_src to test_dst"
+    assert unassoc_line.strip() == "Would unassociate test_unassoc:::: from test_dst"
+
+
+def test_log_pulp_action_no_actions(capsys, set_logging, mock_ubipop_runner):
+    set_logging.addHandler(logging.StreamHandler(sys.stdout))
+    src_repo = get_test_repo(repo_id='test_src')
+    dst_repo = get_test_repo(repo_id='test_dst')
+    associations = [AssociateActionModules([], dst_repo, src_repo)]
+    unassociations = [UnassociateActionModules([], dst_repo)]
+
+    mock_ubipop_runner.log_pulp_actions(associations, unassociations)
+    out, err = capsys.readouterr()
+    assoc_line, unassoc_line = out.split('\n', 1)
+
+    assert err == ""
+    assert assoc_line.strip() == "No association expected for modules from test_src to test_dst"
+    assert unassoc_line.strip() == "No unassociation expected for modules from test_dst"
+
+
+def test_associate_units(mock_ubipop_runner):
+    src_repo = get_test_repo(repo_id='test_src')
+    dst_repo = get_test_repo(repo_id='test_dst')
+    associations = [AssociateActionModules([get_test_mod(name="test_assoc")], dst_repo, src_repo)]
+
+    mock_ubipop_runner.pulp.associate_modules.return_value = ["task_id"]
+    ret = mock_ubipop_runner._associate_unassociate_units(associations)
+
+    assert len(ret) == 1
+    assert ret[0].result() == ["task_id"]
+    assert mock_ubipop_runner._changed_repos == set([dst_repo.repo_id])
