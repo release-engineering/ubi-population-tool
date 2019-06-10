b3363eeda2d6acd28c88b7db9516ebab00935b79
Robert Bikar <rbikar@redhat.com>
2019-06-10 12:13:13 +0200

    Raise to v0.1.18

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def get_requirements():
 
 
 setup(name='ubi-population-tool',
-      version='0.1.17',
+      version='0.1.18',
       license='GNU General Public License',
       author='',
       author_email='',
